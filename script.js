6d0095e9b33ba815d427bce02e9b02a9a521109d
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-26 20:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//your JS code here. If required.
+
+const sizeInfo = document.getElementById("sizeInfo");
 let width = window.innerWidth;
 let height = window.innerHeight;
-const sizeInfo = document.getElementById("sizeInfo")
+
 sizeInfo.textContent = `Width: ${width} and Height: ${height}`
-window.addEventListener("resize",()=>{
-	width = window.innerWidth;;
+
+window.addEventListener('resize', () => {
+	width = window.innerWidth;
 	height = window.innerHeight;
 	sizeInfo.textContent = `Width: ${width} and Height: ${height}`
-})
+	console.log()
+});
